a7e72f2bde4c1bf58aa4c8be11572c6539e71f2f
FrancoDiego21 <franco.diego@iisgalvanimi.edu.it>
2023-05-12 09:25:42 +0000

    Finitio

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AnimalsComponent } from './animals/animals.component';
+import { FruitsComponent } from './fruits/fruits.component';
+import { GenericComponent } from './generic/generic.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'animals', component: AnimalsComponent },
+  { path: 'fruits', component: FruitsComponent},
+  { path:'generic/:id', component: GenericComponent}
+]; //<- Qui vanno inserite tutte le route che desideriamo creare
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
